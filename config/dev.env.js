fec2d0b1116cecad4a03949289ddf63ff9a9ff4b
Manolo Rodriguez <manolo.rodriguez@wizeline.com>
2018-05-11 22:19:34 -0600

    Add authentication handling to App component and env variables

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,9 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  OKTA_URL: '"https://dev-602745.oktapreview.com"',
+  OKTA_CLIENT_ID: '"0oaeyqubq89dv65CQ0h7"',
+  HOST: '"http://localhost"',
+  PORT: '8080'
 })
--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,44 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <nav-bar
+      :authenticated="authenticated"
+      :handleLogout="logout "
+    />
     <router-view/>
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/common/NavBar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: [ NavBar ],
+  data () {
+    return {
+      authenticated: false
+    }
+  },
+  created () {
+    this.isAuthenticated()
+  },
+  watch: {
+    $route: 'isAuthenticated'
+  },
+  methods: {
+    async isAuthenticated () {
+      this.isAuthenticated = await this.$auth.isAuthenticated()
+    },
+    async logout () {
+      await this.$auth.logout()
+      await this.isAuthenticated()
+
+      this.$router.push({
+        path: '/'
+      })
+    }
+  }
+
 }
 </script>
 

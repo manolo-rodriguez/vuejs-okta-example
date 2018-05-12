c81ffc78ed669334d7e0906044589c3547809c7b
Manolo Rodriguez <manolo.rodriguez@wizeline.com>
2018-05-11 21:35:15 -0600

    Create NavBar component

---
+++ src/components/common/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <router-link to="/" tag="button" id='home-button'> Home </router-link>
+    <button v-if='authenticated' v-on:click='logout' id='logout-button'> Logout </button>
+    <button v-else v-on:click='$auth.loginRedirect' id='login-button'> Login </button>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  props: {
+    authenticated: {
+      type: Boolean,
+      default: false
+    },
+    handleLogout: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>

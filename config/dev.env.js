8e37458ba3de72e7628537fd0a470f7a7279236c
Manolo Rodriguez <manolo.rodriguez@wizeline.com>
2018-05-11 23:49:13 -0600

    Change env variables to dotenv-webpack

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,8 +4,4 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  OKTA_URL: '"https://dev-602745.oktapreview.com"',
-  OKTA_CLIENT_ID: '"0oaeyqubq89dv65CQ0h7"',
-  HOST: '"http://localhost"',
-  PORT: '8080'
 })

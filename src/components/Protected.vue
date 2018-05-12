54a1dcd1375d547b6bec1d27d8c08a4ce83db795
Manolo Rodriguez <manolo.rodriguez@wizeline.com>
2018-05-11 20:43:03 -0600

    Create Protected component for the protected route

---
+++ src/components/Protected.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>
+      {{ title }}
+    </h1>
+  </div>  
+</template>
+<script>
+export default {
+  name: 'Protected',
+  data () {
+    return {
+      title: 'This is a protected route!'
+    }
+  }
+}
+</script>
+</script>

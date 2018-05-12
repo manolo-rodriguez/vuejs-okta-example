344752dfa46aef4d0a0f21315f85f8d5b7554e0c
Manolo Rodriguez <manolo.rodriguez@wizeline.com>
2018-05-11 20:31:28 -0600

    Import and use Auth from okta-vue

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+require('dotenv').config()
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Auth from '@okta/okta-vue'
 import Home from '@/components/Home'
 
+Vue.use(Auth, {
+  issuer: `${process.env.OKTA_URL}`,
+  client_id: `${process.env.OKTA_CLIENT_ID}`,
+  redirect_uri: `${process.env.HOST}:${process.env.PORT}/implicit/callback`,
+  scope: 'openid profile email'
+})
 Vue.use(Router)
 
 export default new Router({

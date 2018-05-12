e82004ad1217d6a57f7c72624d4c4f6063bc0cb9
Manolo Rodriguez <manolo.rodriguez@wizeline.com>
2018-05-11 23:01:04 -0600

    Remove some styling from vue cli

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <nav-bar
       :authenticated="authenticated"
       :handleLogout="logout"
@@ -51,8 +50,7 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>

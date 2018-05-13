e7156327abfd0473ef5d2b6429fd0086dcde3eb7
Manolo Rodriguez <manolo.rodriguez@wizeline.com>
2018-05-12 22:56:24 -0600

    Add action to decrement processes in store

--- src/store/modules/progressBar.js
+++ src/store/modules/progressBar.js
@@ -9,12 +9,18 @@ const getters = {
 const actions = {
   startNewProcess ({ commit, state }) {
     commit('incrementProcesses', 1)
+  },
+  finishProcess ({ commit, state }) {
+    commit('decrementProcesses', 1)
   }
 }
 
 const mutations = {
   incrementProcesses (state, numberOfProcesses) {
     state.actionsInProgress = state.actionsInProgress + numberOfProcesses
+  },
+  decrementProcesses (state, numberOfProcesses) {
+    state.actionsInProgress = state.actionsInProgress - numberOfProcesses
   }
 }
 
